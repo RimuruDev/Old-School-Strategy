785b52a49d56ce59443b64c82ecb447d6b88e885
Rimuru <Rimuru.Dev@gmail.com>
2023-09-20 22:48:52 +0400

    Added  A*

---
+++ OldSchoolStrategy/Assets/Internal/Codebase/Runtime/HexagonGridLogic/GridCell.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+namespace PWH.Grids.Examples
+{
+    // Since PathGridObject is abstract, we make inherit it in this script, which does nothing extra.
+
+    public class GridCell : PathGridObject<GridCell>, System.IEquatable<GridCell>
+    {
+        public GridCell(Grid<GridCell> sourceGrid, int xIndex, int yIndex, float movementDifficulty)
+            : base(sourceGrid, xIndex, yIndex, movementDifficulty)
+        {
+        }
+
+        public bool Equals(GridCell other)
+        {
+            if (other == null)
+                throw new NoNullAllowedException(nameof(other));
+
+            return (xIndex == other.xIndex && yIndex == other.yIndex);
+        }
+    }
+}
